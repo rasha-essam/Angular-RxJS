2c11a164af973971dadeed1b175d62ecb81777ff
rasha-essam <engrasha88@gmail.com>
2020-03-12 15:31:40 +0200

    Populate the select item with categories list by listening to an observable, also reacting to user action of selecting specific category by creating an action stream using BehaviorSubject and combine it with the products stream

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,35 +1,59 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
-import { EMPTY } from 'rxjs';
+import { EMPTY, BehaviorSubject, combineLatest } from 'rxjs';
 
 import { ProductService } from './product.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, filter, map } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent  {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories;
+  selectedCategorySubject = new BehaviorSubject<number>(0);
+  selectedCategoryAction$ = this.selectedCategorySubject.asObservable();
 
-  products$ = this.productService.productsWithCategory$.pipe(
+  // products$ = this.productService.productsWithCategory$.pipe(
+  //   catchError(err => {
+  //     this.errorMessage = err;
+  //     return EMPTY;
+  //     // return of([]); // equivalent to return EMPTY
+  //   })
+  // );
+
+  products$ = combineLatest([
+    this.productService.productsWithCategory$,
+    this.selectedCategoryAction$]).pipe(
+    map(([products, selectedCategoryId ]) =>
+      products.filter(product =>
+        selectedCategoryId ? product.categoryId === selectedCategoryId : true
+        )),
+        catchError(err => {
+          this.errorMessage = err;
+          return EMPTY;
+          // return of([]); // equivalent to return EMPTY
+        })
+  );
+
+  categories$ = this.productCategoryService.productCategories$.pipe(
     catchError(err => {
       this.errorMessage = err;
       return EMPTY;
-     // return of([]); // equivalent to return EMPTY
     })
   );
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private productCategoryService: ProductCategoryService) { }
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.selectedCategorySubject.next(+categoryId) ;
   }
 }
